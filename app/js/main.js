60b95ab5cbe7d50f4929ac1b720711300b26d0ae
Dmitiy-hub <dima1997dmytro@gmail.com>
2020-05-22 00:15:39 +0300

    added advantages, productinfo-slider and products sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function() {
-  
+$(function () {
+
+  $('.productinfo-slider__inner').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>'
+  });
+
+  $(".productinfo-slider__item-rate").rateYo({
+    rating: 4.2,
+    readOnly: true,
+    starWidth: "15px"
+  });
+
+  var mixer = mixitup('.products__inner-box');
+
 });
