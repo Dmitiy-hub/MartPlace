ccae1a1ec8ed4eac01ad263aff1dfdfd4aafb863
Dmitiy-hub <dima1997dmytro@gmail.com>
2020-05-24 00:26:12 +0300

    added items, advantagesmart, shopping, feedback, news, services, join and footer sections

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,15 @@ $(function () {
     starWidth: "15px"
   });
 
+  $('.product-slider__inner').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="product__slick-btn slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="product__slick-btn slick-btn slick-next"></button>',
+
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 });
